06b000f4f802c543d2e55bc043916f45a71ce942
Kaitlyn Werden <werden@usc.edu>
2022-12-01 03:39:28 +0000

    Fixing llrec

--- llrec.cpp
+++ llrec.cpp
@@ -4,35 +4,55 @@
 // Provide your implementation of llpivot below
 //*********************************************
 
-void llpivot (Node*& head, Node*& smaller, Node*& larger, int pivot)
+void llpivot(Node*& head, Node*& smaller, Node*& larger, int pivot)
+{
+  smaller = nullptr;
+  larger = nullptr;
+  llpivotHelper(head, smaller, larger, pivot);
+  head = nullptr;
+}
+
+void llpivotHelper (Node*& head, Node*& smaller, Node*& larger, int pivot)
 {
 
 //take the input linked list and separate into smaller and larger linked list
 	
 //base case: when the head node points to null, return
   if (head==NULL)
-  {
+  { 
     return;
   }
 
 //do the work: put the head node in the smaller or larger list
-  Node* oldHead = head;
+  Node* nextHead = head->next;
+  head->next = nullptr;
   if (head->val <= pivot)
   {
-    Node* temp = smaller;
-    smaller = head;
-    smaller->next = temp;
-  
+    if(smaller == nullptr)
+    {
+      smaller = head;
+      llpivotHelper(nextHead, smaller, larger, pivot);
+    }
+    else
+    {
+      smaller->next = head;
+      llpivotHelper(nextHead, smaller->next, larger, pivot);
+    }
   }
-  
   else
   {
-    Node* temp = larger;
-    larger = head;
-    larger->next = temp;
+    if(larger == nullptr)
+    {
+      larger = head;
+      llpivotHelper(nextHead, smaller, larger, pivot);
+    }
+    else
+    {
+      larger->next = head;
+      llpivotHelper(nextHead, smaller, larger->next, pivot);
+    }
   }
-
-//recursive step: go to next head node
-  head = oldHead;
-  llpivot(head, smaller, larger, pivot);
+  
 }
+
+
--- llrec.h
+++ llrec.h
@@ -46,7 +46,8 @@ struct Node
  *   Pivot value
  *
  */
-void llpivot(Node *&head, Node *&smaller, Node *&larger, int pivot);
+void llpivot(Node*& head, Node*& smaller, Node*& larger, int pivot);
+void llpivotHelper (Node*& head, Node*& smaller, Node*& larger, int pivot);
 
 /**
  * Given a linked list pointed to by head, removes (filters out) nodes
